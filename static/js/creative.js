ed6a567933d35cf164f7069ef695011793d7f09a
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-12-13 11:56:47 +0100

    add video with pause button for big devices

--- static/js/creative.js
+++ static/js/creative.js
@@ -45,4 +45,33 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+
+    // Pause button functionality for background video
+    var vid = document.getElementById("bgvid"),
+        pauseButton = document.getElementById("vidpause");
+
+    function vidFade() {
+        vid.classList.add("stopfade");
+    }
+
+    vid.addEventListener('ended', function() {
+        // only functional if "loop" is removed
+         vid.pause();
+        // to capture IE10
+        vidFade();
+    });
+
+    pauseButton.addEventListener("click", function() {
+        vid.classList.toggle("stopfade");
+        if (vid.paused) {
+            vid.play();
+            pauseButton.innerHTML = '<i class="fa fa-pause fa-lg"></i>';
+        } else {
+            vid.pause();
+            pauseButton.innerHTML = '<i class="fa fa-play fa-lg"></i>';
+        }
+    });
+
+
+
 })(jQuery); // End of use strict
